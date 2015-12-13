f8696fdf1e4c5adb5fe3779aeb40a061e82ad85a
Duncan Regan <duncanregan@gmail.com>
2015-12-12 23:48:31 -0500

    creating callbacks for dealing with search and new notes

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,18 +1,47 @@
 import React from 'react';
-import data from '../dummyData'
+import Immutable from 'immutable';
+import data from '../dummyData';
 import Notes from './notes';
+import Header from './header';
 
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
+
+    // TODO: Look in to es7 features
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+
     this.state = {
+      data: data,
       notes: data
     };
+  }
+  handleSearch(searchTerm) {
+    let results = Immutable.List(
+      this.state.data.filter(
+        note => note.get('title').indexOf(searchTerm) !== -1
+    ));
+
+    this.setState(
+      {notes: results}
+    );
+  }
+  handleCreate(title) {
+    let date = new Date();
+    title = title || `Untitled-${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
+
+    let newNote = Immutable.Map({title});
+    // TODO: Add data store with actions
+    let data = this.state.data.unshift(newNote);
+    this.setState(
+      {data, notes: data}
+    );
   }
   render() {
     return (
       <section ref="sidebar" className="sidebar">
-        <header className="sidebar__header"><h1>Notes</h1></header>
+        <Header onSearch={this.handleSearch} onCreate={this.handleCreate} />
         <Notes data={this.state.notes} />
       </section>
     );
