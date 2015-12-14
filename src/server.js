f0bb2b18cff30741385aaf68ac34f997e60b0567
Duncan Regan <duncanregan@gmail.com>
2015-12-14 14:05:00 -0500

    building out the login page

--- src/server.js
+++ src/server.js
@@ -1,2 +1,19 @@
 import express from 'express';
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+// import { Router } from 'react-router';
+// import routes from './routes';
+import cookieParser from 'cookie-parser';
+import ejs from 'ejs';
+
+const server = express();
+server.use(cookieParser())
+server.use(express.static('static'));
+server.engine('html', require('ejs').renderFile)
+
+server.get('/?', (req, res) => {
+  // The login handler
+  return res.render('login.html');
+});
+
+server.listen(7777);
