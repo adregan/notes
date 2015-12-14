edbb4bea6b975cd6c07b889d05b70caa81f2809d
Duncan Regan <duncanregan@gmail.com>
2015-12-14 15:42:16 -0500

    login is now a react component

--- src/server.js
+++ src/server.js
@@ -1,19 +1,32 @@
+import 'babel-register';
+
 import express from 'express';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 // import { Router } from 'react-router';
-// import routes from './routes';
+// import routes from './route;s';
 import cookieParser from 'cookie-parser';
+import compression from 'compression';
 import ejs from 'ejs';
+import Login from '../src/components/login';
 
 const server = express();
 server.use(cookieParser())
 server.use(express.static('static'));
+server.use(compression())
 server.engine('html', require('ejs').renderFile)
 
 server.get('/?', (req, res) => {
-  // The login handler
+  // TODO: Check for cookie from Keybase and forward to app if found
+  // if (cookie) => loading.html else => login.html
+  let login = ReactDOMServer.renderToString(<Login />);
+  return res.render('index.html', {content: login});
+});
+server.post('/?', (req, res) => {
+  // LOGIN
+  // TODO: Check for cookie from Keybase and forward to app if found
   return res.render('login.html');
 });
+
 
 server.listen(7777);
