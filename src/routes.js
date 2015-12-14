059a9e37e64554b4568c463bfadcbc6f67b478ff
Duncan Regan <duncanregan@gmail.com>
2015-12-13 20:53:47 -0500

    using the proper import

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route } from 'react-router';
-import App from './app';
+import App from './handlers/app';
 
 const routes = (
   <Route path="/" component={App}></Route>
