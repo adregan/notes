f12d451bf12caf08709de8c27aed6e63a963bf1a
Duncan Regan <duncanregan@gmail.com>
2015-12-15 15:35:14 -0500

    adds routes and indexes /

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import App from './handlers/app';
+import Login from './handlers/login';
+import Notes from './handlers/notes';
+
+function checkIfLoggedIn(nextState, replaceState) {
+  if (sessionStorage.privateKey) {
+    replaceState({ nextPathname: nextState.location.pathname }, '/notes');
+  }
+}
+
 
 const routes = (
-  <Route path='app' component={App} />
+  <Route path='/' component={App}>
+    <IndexRoute component={Login} onEnter={checkIfLoggedIn}/>
+    <Route path='notes' component={Notes} />
+  </Route>
 );
 
 export default routes;
