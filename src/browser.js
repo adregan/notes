83ed95489e9533bae681d3b2f5cfd6aceef0efa0
Duncan Regan <duncanregan@gmail.com>
2015-12-15 15:33:05 -0500

    using the browser history for non # urls

--- src/browser.js
+++ src/browser.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import routes from './routes';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
-ReactDOM.render(<Router routes={routes} />, document.getElementById('app'))
+const history = createBrowserHistory()
+
+ReactDOM.render(
+  <Router routes={routes} history={history} />,
+  document.getElementById('load')
+)
