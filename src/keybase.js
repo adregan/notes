8a1b6862cdc20511ab8f0d2bff5cf09bcd3df85f
Duncan Regan <duncanregan@gmail.com>
2015-12-14 22:51:14 -0500

    a package for keybase api

---
+++ src/keybase.js
@@ -0,0 +1,69 @@
+import requests from 'superagent';
+import scrypt from 'scrypt';
+import {createHmac} from 'crypto';
+
+export function getSalt(username, password) {
+  let saltURL = 'https://keybase.io/_/api/1.0/getsalt.json';
+  let query = {email_or_username: username};
+
+  return new Promise((resolve, reject) => {
+    requests
+      .get(saltURL)
+      .query(query)
+      .end((err, res) => {
+        if (err) {
+          reject(err);    
+        }
+        let salt = res.body.salt;
+        let session = res.body.login_session;
+        resolve({salt, session, username, password});
+      })
+  })
+}
+
+export function login(options = {}) {
+  if (!Object.keys(options).length) {
+    throw Error('Missing options obj containing salt and session');
+  }
+  let session = options.session;
+  let params = {
+    N: Math.pow(2, 15),
+    r: 8,
+    p: 1
+  };
+
+  let binarySalt = new Buffer(options.salt, 'hex');
+  let decodedSession = new Buffer(session, 'base64');
+
+  let pwh = scrypt.hashSync(
+    options.password, params, 224, binarySalt).slice(192, 224);
+
+  let hmac = createHmac('sha512', pwh);
+  hmac.write(decodedSession);
+  let hmacPWH = hmac.digest('hex');
+
+  let loginURL = 'https://keybase.io/_/api/1.0/login.json';
+  let query = {
+    email_or_username: options.username,
+    hmac_pwh: hmacPWH,
+    login_session: session
+  }
+  return new Promise((resolve, reject) => {
+    requests
+      .post(loginURL)
+      .query(query)
+      .end((err, res) => {
+        if (err) {
+          reject(err);
+        }
+        if (res.body.me) {
+          // TODO: Also return private and public keys
+          let privateKey = res.body.me.private_keys.primary;
+          let publicKey = res.body.me.public_keys.primary;
+          let cookies = res.headers['set-cookie'];
+          resolve({privateKey, publicKey, cookies})
+        }
+        reject(Error('Login unsuccessful.'))
+      })
+  })
+}
