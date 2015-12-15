5aa72aeb58ea4c1fb64f9aaeb8a3ddc29176c63f
Duncan Regan <duncanregan@gmail.com>
2015-12-15 15:33:54 -0500

    app is now the app handler

--- src/handlers/app.js
+++ src/handlers/app.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Sidebar from '../components/sidebar';
-import Editor from '../components/editor';
 
 class App extends React.Component {
-  // componentDidMount() {
-  //   // TODO: Connect this to unsaved changes
-  //   window.onbeforeunload = () => 'Are you sure?';
-  // }
   render() {
     return (
-      <div>
-        <Sidebar />
-        <Editor />
+      <div className="app-window">
+        {this.props.children}
       </div>
     );
   }
