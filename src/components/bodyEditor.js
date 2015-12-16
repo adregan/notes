efddf7676218eb9ce18b5437788f1f0660cc6fcc
Duncan Regan <duncanregan@gmail.com>
2015-12-16 17:35:45 -0500

    forces forcus on the editor

--- src/components/bodyEditor.js
+++ src/components/bodyEditor.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 class SimpleEditor extends React.Component {
   handleChange(event) {
     this.props.onChange({key: 'body', value: event.target.value});
+  }
+  componentDidMount() {
+    ReactDom.findDOMNode(this).focus();
   }
   render() {
     return (
