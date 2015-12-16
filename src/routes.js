ea06d98f8fa7d099d3b7ce122374294b9562e098
Duncan Regan <duncanregan@gmail.com>
2015-12-16 17:36:53 -0500

    fixes the route

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,21 @@ import { Route, IndexRoute } from 'react-router';
 import App from './handlers/app';
 import Login from './handlers/login';
 import Notes from './handlers/notes';
+import Editor from './handlers/editor';
 
 function checkIfLoggedIn(nextState, replaceState) {
-  if (sessionStorage.privateKey) {
-    replaceState({ nextPathname: nextState.location.pathname }, '/notes');
-  }
+  // if (sessionStorage.privateKey) {
+  //   replaceState({ nextPathname: nextState.location.pathname }, '/notes');
+  // }
 }
 
 
 const routes = (
   <Route path='/' component={App}>
     <IndexRoute component={Login} onEnter={checkIfLoggedIn}/>
-    <Route path='notes' component={Notes} />
+    <Route path='notes' component={Notes}>
+      <Route path=':id' component={Editor} />
+    </Route>
   </Route>
 );
 
