157c1b1e0351b1bfc549ea6ffc575f755765bb93
Duncan Regan <duncanregan@gmail.com>
2015-12-16 17:36:14 -0500

    updates input to be a text input

--- src/components/titleEditor.js
+++ src/components/titleEditor.js
@@ -1,36 +1,25 @@
 import React from 'react';
 
-class FancyInput extends React.Component {
+class TitleEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.handlePaste = this.handlePaste.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-  }
-  handlePaste(event) {
-    event.preventDefault();
-    let newText = event.clipboardData.getData('text')
-                        .replace(/(\r\n|\n|\r)/gm, '')
-                        .substring(0, this.props.limit);
-
-    this.props.onChange({key: 'title', text: newText})
   }
   handleInput(event) {
     event.preventDefault();
-    let newText = event.target.textContent;
+    let newText = event.target.value;
     if (newText.length < this.props.limit) {
       this.props.onChange({key: 'title', text: newText})
     }
   }
   render() {
     return (
-      <h2 
+      <input
+        type="text" 
         className={this.props.className}
-        contentEditable="true"
-        onPaste={this.handlePaste}
-        onInput={this.handleInput}
-        dangerouslySetInnerHTML={{__html: this.props.data}}/>
+        onInput={this.handleInput.bind(this)}
+        value={this.props.data}/>
     );
   }
 }
 
-export default FancyInput;
+export default TitleEditor;
