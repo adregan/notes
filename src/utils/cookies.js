901021e9723070edea3c24eda140acd710c4b7df
Duncan Regan <duncanregan@gmail.com>
2015-12-17 02:13:19 -0500

    adds functions to help encrypt and decrypt cookies

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,4 +1,6 @@
-export default function createCookieDict(cookieString) {
+import crypto from 'crypto';
+
+export function createCookieDict(cookieString) {
   let cookie = {
     options: {}
   }
@@ -22,4 +24,25 @@ export default function createCookieDict(cookieString) {
   })
 
   return cookie;
+}
+
+export const encryptedCookie = {
+  encrypt: function(token, key) {
+    let cipher = crypto.createCipher('aes256', key);
+    let encrypted = cipher.update(token, 'utf8', 'base64') + cipher.final('base64');
+
+    return encrypted;
+  },
+  decrypt: function(token, key) {
+    let decipher = crypto.createDecipher('aes256', key);
+    let decrypted = decipher.update(token, 'base64', 'utf8') + decipher.final('utf8');
+
+    return decrypted;
+  },
+  setExpiration: function(days) {
+    let date = new Date();
+    let expirationDate = new Date(date.setDate(date.getDate() + days));
+
+    return expirationDate;
+  }
 }
