4c3e4ee9a5ae3b5dd4bf289d92ee931ef519d4b5
Duncan Regan <duncanregan@gmail.com>
2015-12-16 22:19:55 -0500

    removing this

--- src/handlers/notes.js
+++ src/handlers/notes.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sidebar from '../components/sidebar';
-//import Editor from '../components/editor';
 
 class Notes extends React.Component {
-  // componentDidMount() {
-  //   // TODO: Connect this to unsaved changes
-  //   window.onbeforeunload = () => 'Are you sure?';
-  // }
   render() {
     return (
       <article className="notes">
