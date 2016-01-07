2eb634c5a3039b7247aa55a011df8562539a3558
Duncan Regan <duncanregan@gmail.com>
2016-01-06 21:49:27 -0500

    creating stateless components

--- src/js/components/notes.js
+++ src/js/components/notes.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
-class NotesList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <ul className="notes-list">
-        {this.props.data.map((note, i) => {
-          return (
-            <li key={i}>{note.get('title', 'Untitled')}</li>
-          );
-        })}
-      </ul>
-    );
-  }
+const NotesList = ({data}) => {
+  return (
+    <ul className="notes-list">
+      {data.map((note, i) => {
+        return (
+          <li key={i}>{note.get('title', 'Untitled')}</li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default NotesList; 
--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -2,20 +2,13 @@ import React from 'react';
 import NotesList from './notes';
 import Header from './header';
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <section ref="sidebar" className="sidebar">
-        <Header 
-          onSearch={this.props.onSearch} 
-          onCreate={this.props.onCreate} />
-        <NotesList data={this.props.notes} />
-      </section>
-    );
-  }
+const Sidebar = ({onSearch, notes}) => {
+  return (
+    <section className="sidebar">
+      <Header onSearch={onSearch} />
+      <NotesList data={notes} />
+    </section>
+  );
 }
 
 export default Sidebar;
