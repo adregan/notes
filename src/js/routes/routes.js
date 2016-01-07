2d28076446c8de53826fee22852955c8dcd44538
Duncan Regan <duncanregan@gmail.com>
2016-01-06 23:46:19 -0500

    removing the editor route

--- src/js/routes/routes.js
+++ src/js/routes/routes.js
@@ -4,15 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './app';
 import Login from './login';
 import Notes from './notes';
-import Editor from './editor';
 
 const routes = (
   <Route path='/' component={App}>
     <IndexRoute component={Login} />
     <Route path='login' component={Login} />
-    <Route path='notes' component={Notes}>
-      <Route path=':id' component={Editor} />
-    </Route>
+    <Route path='notes' component={Notes} />
   </Route>
 );
 
