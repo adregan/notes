4a3ad681d052948263b86ef0081ef861c7fda060
Duncan Regan <duncanregan@gmail.com>
2016-01-11 21:28:21 -0500

    simplifies the login route handler
    none of the login values are available from the store to keep anything sensitive from leaking into or out of

--- src/js/routes/login.js
+++ src/js/routes/login.js
@@ -1,40 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginForm from '../components/loginForm';
-import fetch from '../utils/fetch';
-import cookie from 'react-cookie';
+import Logo from '../components/logo';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: this.props.username,
-      password: this.props.password,
+      username: '',
+      password: '',
       error: false
     }
   }
-  handleChange(options) {
-    this.setState({
-      [options.key]: options.value
-    })
-  }
-  handleSubmit(options) {
-    event.preventDefault();
-    let { username, password } = options;
-    fetch('/login', {method: 'post', body: {username, password}})
-      .then(resp => {
-        let publicKey = resp.publicKey;
-        let privateKey = resp.privateKey;
-
-        cookie.save('notes', resp.notesCookie);
-        this.props.history.pushState(null, 'notes');
-      })
-      .catch(err => {
-        console.error(err);
-        // Assign this conditionally on err.response
-        let error = (!err.response) ? 'So sorry. Something went wrong.' : err.response.body.error;
-        this.setState({error});
-      })
+  handleSubmit() {
+    let { username, password } = this.state;
   }
   render() {
     return (
@@ -46,7 +25,7 @@ class Login extends React.Component {
           <LoginForm 
             username={this.state.username} 
             password={this.state.password}
-            onChange={this.handleChange.bind(this)}
+            onChange={(change) => this.setState(change)}
             onSubmit={this.handleSubmit.bind(this)} />
         </article>
       </div>
