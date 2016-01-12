906ad782366cc54b865640b5a1afaf7d3b58bbb1
Duncan Regan <duncanregan@gmail.com>
2016-01-11 23:12:05 -0500

    logging in via the new api

--- src/js/routes/login.js
+++ src/js/routes/login.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginForm from '../components/loginForm';
 import Logo from '../components/logo';
+import { api } from '../../../config';
+import fetch from '../utils/fetch';
+import { connect } from 'react-redux';
+import { logIn } from '../store/actions';
 
 class Login extends React.Component {
   constructor(props) {
@@ -9,11 +13,28 @@ class Login extends React.Component {
     this.state = {
       username: '',
       password: '',
-      error: false
+      error: false,
+      loggingIn: false
     }
   }
   handleSubmit() {
     let { username, password } = this.state;
+    let apiUrl = (window.location.hostname === 'localhost') ? api.dev : api.production;
+    let loginUrl = `${apiUrl}/login`;
+
+    this.setState({loggingIn: true});
+
+    fetch(loginUrl, {method: 'post', body: {username, password}})
+      .then(resp => {
+        this.props.dispatch(logIn(resp));
+        this.setState({error: false, loggingIn: false, username: '', password: ''});
+      })
+      .catch(err => {
+        console.error(err);
+        // Assign this conditionally on err.response
+        let error = (!err.response) ? 'So sorry. Something went wrong.' : err.response.body.errorMessage;
+        this.setState({loggingIn: false, error});
+      })
   }
   render() {
     return (
@@ -25,6 +46,7 @@ class Login extends React.Component {
           <LoginForm 
             username={this.state.username} 
             password={this.state.password}
+            loggingIn={this.state.loggingIn}
             onChange={(change) => this.setState(change)}
             onSubmit={this.handleSubmit.bind(this)} />
         </article>
@@ -33,4 +55,4 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+export default connect()(Login);
