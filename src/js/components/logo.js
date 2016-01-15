bc6c6fec5a49deb31cbf4256b10fd42078c9e37c
Duncan Regan <duncanregan@gmail.com>
2016-01-14 23:18:16 -0500

    sets the static path on the resources

--- src/js/components/logo.js
+++ src/js/components/logo.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { staticPath } from '../../../config';
 
 const Logo = ({color, className}) => {
   let svg = (color === 'light') ? 'logolight.svg' : 'logo.svg'
   let fallback = (color === 'light') ? 'logolight.png' : 'logo.png'
   return (
-    <object type="image/svg+xml" data={svg} className={className}>
-      <img src={fallback} alt="Notes" />
+    <object type="image/svg+xml" data={staticPath + svg} className={className}>
+      <img src={staticPath + fallback} alt="Notes" />
     </object>
   );
 }
