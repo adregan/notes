065c1d73ef9a7769f8c265f30efd3cb31b634667
Duncan Regan <duncanregan@gmail.com>
2016-01-18 17:18:02 -0500

    moves the loggingIn state out of the user store

--- src/js/reducers/user.js
+++ src/js/reducers/user.js
@@ -1,10 +1,23 @@
 import Immutable from 'immutable';
-import {LOGGING_IN, STORE_USER, UPDATE_USER, LOG_OUT} from '../actions/user';
+import {STORE_USER, UPDATE_USER, LOG_OUT} from '../actions/user';
 
-const user = (state = Immutable.Map({loggingIn: false, username: ''}), action) => {
+/*
+=====================================
+      DIAGRAM OF THE USER STORE
+=====================================
+
+user: {
+  name: <string>,
+  email: <string>,
+  apiToken: <string>,
+  armoredKey: <string>,
+  photoUrl: <string>
+}
+
+*/
+
+const user = (state = Immutable.Map(), action) => {
   switch (action.type) {
-    case LOGGING_IN:
-      return state.set('loggingIn', !state.get('loggingIn'));
     case STORE_USER:
       return state.merge(action.user);
     case UPDATE_USER:
