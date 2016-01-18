a3652efa9e85e1b8124dee0fa79d0f45693c79a3
Duncan Regan <duncanregan@gmail.com>
2016-01-18 17:18:38 -0500

    renames search

---
+++ src/js/reducers/search.js
@@ -0,0 +1,28 @@
+import Immutable from 'immutable';
+import {SEARCH} from '../actions/search';
+import {ADD_NOTE} from '../actions/notes';
+import {LOG_OUT} from '../actions/user';
+
+/*
+=====================================
+      DIAGRAM OF THE SEARCH STORE
+=====================================
+
+search: {
+  term: <string>,
+}
+
+*/
+
+const search = (state = Immutable.Map({term: ''}), action) => {
+  switch (action.type) {
+    case SEARCH:
+      return state.set('term', action.term);
+    case LOG_OUT:
+      return state.clear();
+    default:
+      return state;
+  }
+}
+
+export default search;
