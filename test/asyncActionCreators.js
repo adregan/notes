708963af6ca338994e7da4a8bdc84779f2a030cb
Duncan Regan <duncanregan@gmail.com>
2016-01-18 21:34:52 -0500

    tools for async tests + scaffold for async tests

---
+++ test/asyncActionCreators.js
@@ -0,0 +1,56 @@
+import expect from 'expect';
+
+describe('Async Action Creators', () => {
+  describe('notes', () => {
+    describe('selectNote()', () => {
+      it('should create an action to select a note', () => {
+
+      })
+    })
+    describe('updateNote()', () => {
+      it('should create an action to update a note', () => {
+
+      })
+    })
+    describe('saveNote()', () => {
+      it('should create an action to save a note', () => {
+
+      })
+    })
+    describe('deleteNote()', () => {
+      it('should create an action to delete a note', () => {
+
+      })
+    })
+  })
+
+  describe('users', () => {
+    describe('logIn()', () => {
+      it('should create an action to log a user in', () => {
+
+      })
+    })
+    describe('checkForCurrentSession()', () => {
+      it('should create an action to check for a current session', () => {
+
+      })
+    })
+    describe('storeUser()', () => {
+      it('should create an action to store a user', () => {
+
+      })
+    })
+    describe('unlock()', () => {
+      it('should create an action to unlock a user\'s key', () => {
+
+      })
+    })
+    describe('disconnect()', () => {
+      it('should create an action to disconnect a user', () => {
+
+      })
+    })
+
+  })
+})
+
