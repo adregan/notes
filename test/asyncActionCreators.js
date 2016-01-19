d778821270b5c7e5c734c0a491972a349be22d8d
Duncan Regan <duncanregan@gmail.com>
2016-01-18 23:31:26 -0500

    setting up async tests with mock data

--- test/asyncActionCreators.js
+++ test/asyncActionCreators.js
@@ -1,23 +1,53 @@
+import Immutable from 'immutable';
 import expect from 'expect';
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import uuid from 'node-uuid';
+import * as notes from '../src/js/actions/notes';
+
+const middlewares = [ thunk ]
+const mockStore = configureMockStore(middlewares)
 
 describe('Async Action Creators', () => {
   describe('notes', () => {
-    describe('selectNote()', () => {
-      it('should create an action to select a note', () => {
+    const mockIds = (new Array(20)).join(',').split(',').map((_, i) => uuid.v4());
+    const mockNotes = Immutable.List(mockIds.map((id, i) => Immutable.Map({
+      id: id,
+      content: '',
+      saved: false,
+      decrypted: {
+        title: `Note ${i + 1}`,
+        body: 'WOWOWOWOWOWOWWOWOOW'
+      }
+    })));
 
+    describe('selectNote()', (done) => {
+      it('should create an action with the selected note', () => {
+        const note = mockNotes.get(16);
+        const theID = note.get('id');
+
+        const expectedActions = [{type: notes.SELECT_NOTE, note}];
+
+        const store = mockStore(
+          { notes: mockNotes },
+          expectedActions,
+          done
+        );
+        
+        store.dispatch(notes.selectNote(theID));
       })
     })
-    describe('updateNote()', () => {
+    describe('updateNote()', (done) => {
       it('should create an action to update a note', () => {
 
       })
     })
-    describe('saveNote()', () => {
+    describe('saveNote()', (done) => {
       it('should create an action to save a note', () => {
 
       })
     })
-    describe('deleteNote()', () => {
+    describe('deleteNote()', (done) => {
       it('should create an action to delete a note', () => {
 
       })
