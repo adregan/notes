72eca014c65fc2b190176c52dfdcac84229c7cec
Duncan Regan <duncanregan@gmail.com>
2016-01-26 17:11:34 -0500

    a link to the demo route from the log in page

--- src/js/routes/login.js
+++ src/js/routes/login.js
@@ -3,16 +3,16 @@ import LoginForm from '../components/loginForm';
 import Logo from '../components/logo';
 import { startDemo } from '../actions/demo';
 import { connect } from 'react-redux';
+import history from './history';
 
 const Login = ({dispatch}) => {
   return (
     <div>
       <Logo color="light" className="login-logo"/>
       <article className="login">
-        <p>You must have a Keybase.io account to use this app</p>
+        <p>You must have a Keybase.io account to use this app.</p>
+        <p>However, you can <a href='/demo' onClick={e => {e.preventDefault(); history.push('/demo/')}}>demo it here</a>.</p>
         <LoginForm />
-
-        <button className="demo-button" onClick={() => dispatch(startDemo())}>Demo</button>
       </article>
     </div>
   );
