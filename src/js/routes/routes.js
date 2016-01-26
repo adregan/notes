ff123d751a6e93f172d760b901cf0207653ed233
Duncan Regan <duncanregan@gmail.com>
2016-01-26 00:49:30 -0500

    removing debug log

--- src/js/routes/routes.js
+++ src/js/routes/routes.js
@@ -8,7 +8,6 @@ import Settings from './settings';
 import localforage from 'localforage';
 
 const checkForPreviousSession = (next, replace, callback) => {
-  console.log('You don\'t want to see me more that once really');
   let path = next.location.pathname.replace(/\//g, '');
   localforage.getItem('user')
     .then(user => {
