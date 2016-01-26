f864526cd75bf4a325a5b10f4165b8e4e53f7383
Duncan Regan <duncanregan@gmail.com>
2016-01-26 15:47:55 -0500

    adds a demo button to the login panel
    issue #21 issue #16

--- src/js/routes/login.js
+++ src/js/routes/login.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import LoginForm from '../components/loginForm';
 import Logo from '../components/logo';
+import { startDemo } from '../actions/demo';
+import { connect } from 'react-redux';
 
-const Login = () => {
+const Login = ({dispatch}) => {
   return (
     <div>
       <Logo color="light" className="login-logo"/>
       <article className="login">
         <p>You must have a Keybase.io account to use this app</p>
         <LoginForm />
+
+        <button className="demo-button" onClick={() => dispatch(startDemo())}>Demo</button>
       </article>
     </div>
   );
 }
 
-export default Login;
+export default connect()(Login);
